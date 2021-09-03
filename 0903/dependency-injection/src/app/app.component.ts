69d3461dd6551df2b9c86e975f9c5a51fa9f7c00
정이삭 <su951100@gmail.com>
2021-09-03 18:13:35 +0900

    의존성 주입 연습

---
+++ 0903/dependency-injection/src/app/app.component.ts
@@ -0,0 +1,25 @@
+// app.component.ts
+import { Component } from '@angular/core';
+import { GreetingService } from './greeting.service';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <button (click)="sayHi()">Say Hi</button>
+    <p>{{ greeting }}</p>
+  `
+})
+export class AppComponent {
+  greeting!: string;
+  greetingService: GreetingService;
+
+  constructor() {
+    // 서비스의 인스턴스를 직접 생성한다.
+    this.greetingService = new GreetingService();
+  }
+
+  sayHi() {
+    // 서비스의 사용
+    this.greeting = this.greetingService.sayHi();
+  }
+}
