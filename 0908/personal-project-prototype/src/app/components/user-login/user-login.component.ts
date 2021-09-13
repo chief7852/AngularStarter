b3537bd96a114c6cd1f6996d4ce989912255de39
정이삭 <su951100@gmail.com>
2021-09-13 19:31:31 +0900

    prototype

--- 0908/personal-project-prototype/src/app/components/user-login/user-login.component.ts
+++ 0908/personal-project-prototype/src/app/components/user-login/user-login.component.ts
@@ -1,31 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, observable, Subscriber } from 'rxjs';
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
+  allowId = 'asas';
+  allowpass = 'asas'
+  userPassword = '';
+  userId = '';
 
-  
   form = this.formBuilder.group({
     userId: '',
     userPassword: '',
   });
   constructor(
-    private formBuilder : FormBuilder
-  ){
+    private formBuilder: FormBuilder,
+    private router : Router
+  ) {
 
 
-    
+
   }
   ngOnInit(): void {
+    this.form.valueChanges.subscribe(newVlaue => {
+      this.allowId = '';
+      this.allowpass = '';
+    })
   }
 
   onSubmit($event: any) {
-    console.log(this.form.value.userId);
-    console.log(this.form.value.userPassword);
 
+    if (this.form.value.userId === 'asas' && this.form.value.userPassword === 'asas') {
+      alert("로그인 성공");
+      this.router.navigate(['/main']);
+    }else{
+      alert("해당정보없음");
+    }
+
+    this.form.reset();
   }
+
+
+
+
+
 }
