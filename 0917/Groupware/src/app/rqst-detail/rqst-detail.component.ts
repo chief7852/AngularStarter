847d8cb01b3f58c00dba8a4de461dbebd68776d5
ISSAC7852 <su951100@gmail.com>
2021-09-18 22:52:35 +0900

    전자결재 예시 5%

--- 0917/Groupware/src/app/rqst-detail/rqst-detail.component.ts
+++ 0917/Groupware/src/app/rqst-detail/rqst-detail.component.ts
@@ -1,4 +1,6 @@
+import { DirectiveResolver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-rqst-detail',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RqstDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+        this.form = this.fb.group({
+      REG_DT: [''],
+      FROM_DT: ['2020-01-01'],
+      TO_DT: ['2020-01-03'],
+      CLS: [''],
+      AUDIT_STAT: [''],
+      SBJT: [''],
+    });
   }
+
+
+ onClick(){
+   DirectiveResolver be  = new Component;
+
+   ang
+ }
 
 }
