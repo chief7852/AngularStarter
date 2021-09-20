0bc6a1e7f30540ca64f899ad2c5caf072729a843
ISSAC7852 <su951100@gmail.com>
2021-09-20 23:00:39 +0900

    materal module install and alter

--- angular-website/src/app/modules/material/material.module.ts
+++ angular-website/src/app/modules/material/material.module.ts
@@ -1,45 +1,14 @@
-// material.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import {
-  MatSidenavModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatListModule,
-  MatCardModule,
-  MatButtonModule,
-  MatTableModule,
-  MatInputModule,
-  MatDialogModule,
-  MatSliderModule
-} from '@angular/material';
+  Mat
+}
 
 @NgModule({
+  declarations: [],
   imports: [
-    CommonModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatDialogModule,
-    MatSliderModule
-  ],
-  exports: [
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatDialogModule,
-    MatSliderModule
-  ],
-  declarations: []
+    CommonModule
+  ]
 })
 export class MaterialModule { }
