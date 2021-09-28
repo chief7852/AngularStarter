7f7295ffe705036027e4dfcc568c7ca88d1c5164
정이삭 <su951100@gmail.com>
2021-09-28 18:11:36 +0900

    calendar 분해

--- 0927/angular-template-group/src/app/demo/pages/authentication/auth-signup/auth-signup.module.ts
+++ 0927/angular-template-group/src/app/demo/pages/authentication/auth-signup/auth-signup.module.ts
@@ -5,6 +5,7 @@ import { AuthSignupRoutingModule } from './auth-signup-routing.module';
 import { AuthSignupComponent } from './auth-signup.component';
 import { ModalModule } from 'src/app/theme/shared/components';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbCalendarHebrew } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
     ModalModule,
     FormsModule,
     ReactiveFormsModule,
+    NgbCalendarHebrew
   ],
   declarations: [AuthSignupComponent]
 })
