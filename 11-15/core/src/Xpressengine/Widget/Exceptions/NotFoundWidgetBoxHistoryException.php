ff0fab690b02fb1a318de05fb02c4494bb21a5c6
정이삭 <su951100@gmail.com>
2021-11-16 17:59:11 +0900

    fk php

--- 11-15/core/src/Xpressengine/Widget/Exceptions/NotFoundWidgetBoxHistoryException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Xpressengine\Widget\Exceptions;
-
-use Xpressengine\Widget\WidgetException;
-
-class NotFoundWidgetBoxHistoryException extends WidgetException
-{
-}
--- 11-15/resources/views/terms/index.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div  style="margin:20px 0;">
-        <h2>{{ xe_trans($term->title) }}</h2>
-        <hr />
-        <div style="padding:20px; background:#eee; border:1px solid #ccc;">
-            {!! nl2br(xe_trans($term->content)) !!}
-        </div>
-    </div>
-</div>
